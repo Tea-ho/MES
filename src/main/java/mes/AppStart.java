523271fc24d168105f32b2ee81b9eaa345bac67a
Ga0Kwon <kgayeong39@gmail.com>
2023-05-02 14:43:52 +0900

    Initial commit Spring

---
+++ src/main/java/mes/AppStart.java
@@ -0,0 +1,14 @@
+package mes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@SpringBootApplication // 하위 폴더내 컴포넌트 스캔 빈 등록
+@EnableJpaAuditing //BaseTime의 @EntityListeners(AuditingEntityListener.class)와 세트
+public class AppStart {
+    public static void main(String[] args) {
+        //dddddddddddddddddd
+        SpringApplication.run(AppStart.class);
+    }
+}

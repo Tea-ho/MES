525123d9750b686ff4d49cbf710a64f48c261afb
Ga0Kwon <kgayeong39@gmail.com>
2023-05-10 14:35:58 +0900

    05/10 정준님 병합

--- src/main/java/mes/AppStart.java
+++ src/main/java/mes/AppStart.java
@@ -8,7 +8,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @EnableJpaAuditing //BaseTime의 @EntityListeners(AuditingEntityListener.class)와 세트
 public class AppStart {
     public static void main(String[] args) {
-
+        //정준님 병합
         SpringApplication.run(AppStart.class);
 
     }

a0f88f5f375ac78e6d750eb959ca86eb3b5f941b
Tea-ho <hokeng92@naver.com>
2023-05-16 14:46:10 +0900

    23.05.13, 17:41 1) ProductProcessEntity 데이터 검토

--- src/main/java/mes/domain/dto/performance/ProductProductionDto.java
+++ src/main/java/mes/domain/dto/performance/ProductProductionDto.java
@@ -1,6 +1,16 @@
 package mes.domain.dto.performance;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data@NoArgsConstructor@AllArgsConstructor@Builder
 public class ProductProductionDto {
     private String productName;
-    private int productionCount;
+    private long prodPrice;
+    private long averageProductionCount;
+    private long totalProductionCount;
+    private long totalProductionAmount;
+    private double productionPercentage;
 }

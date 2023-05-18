65048b305e171cdafc9adb35f3e6546abba5e60d
Tea-ho <hokeng92@naver.com>
2023-05-18 17:43:59 +0900

    23.05.18, 17:43 제품 재고 누적 더하기 적용(기재고 있을 경우)

--- src/main/java/mes/service/member/AllowApprovalService.java
+++ src/main/java/mes/service/member/AllowApprovalService.java
@@ -132,19 +132,32 @@ public class AllowApprovalService {
             materialInOutEntity.ifPresent(entity -> updateAllowApproval(entity.getAllowApprovalEntity(), false, session));
         } return true;
     }
+    // 제품 누적 추가로 코드 변경 [23.05.18, th]
     public boolean approveProductInOut(List<Integer> ProdInOutIDs, HttpSession session) {
         for (int id : ProdInOutIDs) {
             Optional<ProductPlanEntity> productPlanEntity = productPlanRepository.findById(id);
             productPlanEntity.ifPresent(entity -> {
                 updateAllowApproval(entity.getAllowApprovalEntity(), true, session);
-                ProductProcessEntity productProcessEntity = new ProductProcessEntity();
 
-                productProcessEntity.setProductEntity(entity.getProductEntity());
-                productProcessEntity.setProdStock(Integer.parseInt(entity.getProdPlanCount()));
-                productProcessEntity.setProdProcDate(LocalDate.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd")));
+                ProductProcessEntity productProcessEntity = productProcessRepository.findByProductEntity(entity.getProductEntity());
+
+                if (productProcessEntity != null) {
+                    // 동일한 정보가 이미 존재하는 경우
+                    int existingStock = productProcessEntity.getProdStock();
+                    int newStock = existingStock + Integer.parseInt(entity.getProdPlanCount());
+                    productProcessEntity.setProdStock(newStock);
+                } else {
+                    // 새로운 정보를 생성하는 경우
+                    productProcessEntity = new ProductProcessEntity();
+                    productProcessEntity.setProductEntity(entity.getProductEntity());
+                    productProcessEntity.setProdStock(Integer.parseInt(entity.getProdPlanCount()));
+                    productProcessEntity.setProdProcDate(LocalDate.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd")));
+                }
+
                 productProcessRepository.save(productProcessEntity);
             });
-        } return true;
+        }
+        return true;
     }
     public boolean rejectProductInOut(List<Integer> ProdInOutIDs, HttpSession session) {
         for (int id : ProdInOutIDs) {

66d3c2033894c2f5aca2c71910755de95b9b6159
Ga0Kwon <kgayeong39@gmail.com>
2023-05-19 14:35:25 +0900

    05/19 Ga0Kwon merge

--- src/main/java/mes/AppStart.java
+++ src/main/java/mes/AppStart.java
@@ -8,7 +8,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @EnableJpaAuditing //BaseTime합의 @EntityListeners(AuditingEntityListener.class)와 세트
 public class AppStart {
     public static void main(String[] args) {
-        //05/19 Seong Ah
+        //05/19 Ga0Kwon
         SpringApplication.run(AppStart.class);
 
     }
